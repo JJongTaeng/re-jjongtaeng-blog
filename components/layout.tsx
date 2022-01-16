57f6491769ecfd217340fd21d4edee8d03451392
쫑탱 <jjojjo141995@gmail.com>
2022-01-16 22:20:17 +0900

    title, name 변경

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,8 +4,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Your Name'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'JJongTaeng'
+export const siteTitle = `JJongTaeng's Blog`
 
 export default function Layout({ children, home }: any) {
   return (
